75bb915d2b0387052d829f892ff6a0e7b250c063
glit <hi@glit.ltd>
2025-08-06 00:14:58 +0800

    fix build error

--- app/public/src/pages/utils/date.ts
+++ app/public/src/pages/utils/date.ts
@@ -1,31 +1,41 @@
-import i18n from "../../i18n"
+import i18n from "../../i18n";
 
 export function formatDate(
   date: number | Date,
   params: Intl.DateTimeFormatOptions = {}
 ) {
-  if (typeof date === "number") date = new Date(date)
+  if (typeof date === "number") date = new Date(date);
   try {
     return new Intl.DateTimeFormat(i18n.language, {
       dateStyle: "short",
       timeStyle: "short",
-      ...params
-    }).format(date)
+      ...params,
+    }).format(date);
   } catch (err) {
-    return "Invalid Date"
+    return "Invalid Date";
   }
 }
 
 export function formatDuration(seconds: number) {
-
-  const days = Math.floor(seconds / 86400)
-  seconds -= days * 86400
-  const hours = Math.floor(seconds / 3600)
-  seconds -= hours * 3600
-  const minutes = Math.floor(seconds / 60)
-  seconds -= minutes * 60
+  const days = Math.floor(seconds / 86400);
+  seconds -= days * 86400;
+  const hours = Math.floor(seconds / 3600);
+  seconds -= hours * 3600;
+  const minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+  // @ts-ignore
   if (Intl && Intl.DurationFormat) {
-    return new Intl.DurationFormat(i18n.language, { style: "long" }).format({ days, hours, minutes, seconds });
+    // @ts-ignore
+    return new Intl.DurationFormat(i18n.language, { style: "long" }).format({
+      days,
+      hours,
+      minutes,
+      seconds,
+    });
   }
-  return `${days > 0 ? days + " days" : ""}${hours > 0 ? hours + " hours" : ""}${minutes > 0 ? minutes + " min" : ""}${seconds > 0 ? seconds + " s" : ""}`.trim()
+  return `${days > 0 ? days + " days" : ""}${
+    hours > 0 ? hours + " hours" : ""
+  }${minutes > 0 ? minutes + " min" : ""}${
+    seconds > 0 ? seconds + " s" : ""
+  }`.trim();
 }
--- app/utils/orientation.ts
+++ app/utils/orientation.ts
@@ -1,6 +1,6 @@
-import Board, { Cell } from "../core/board"
-import { PokemonEntity } from "../core/pokemon-entity"
-import { Orientation } from "../types/enum/Game"
+import { Board, Cell } from "../core/board";
+import { PokemonEntity } from "../core/pokemon-entity";
+import { Orientation } from "../types/enum/Game";
 
 export const OrientationVector: Record<Orientation, [number, number]> = {
   [Orientation.UP]: [0, 1],
@@ -10,8 +10,8 @@ export const OrientationVector: Record<Orientation, [number, number]> = {
   [Orientation.DOWN]: [0, -1],
   [Orientation.DOWNLEFT]: [-1, -1],
   [Orientation.LEFT]: [-1, 0],
-  [Orientation.UPLEFT]: [-1, 1]
-}
+  [Orientation.UPLEFT]: [-1, 1],
+};
 
 export const OrientationAngle: Record<Orientation, number> = {
   [Orientation.UP]: Math.PI / 2,
@@ -19,10 +19,10 @@ export const OrientationAngle: Record<Orientation, number> = {
   [Orientation.RIGHT]: 0,
   [Orientation.DOWNRIGHT]: -Math.PI / 4,
   [Orientation.DOWN]: -Math.PI / 2,
-  [Orientation.DOWNLEFT]: -3 * Math.PI / 4,
+  [Orientation.DOWNLEFT]: (-3 * Math.PI) / 4,
   [Orientation.LEFT]: Math.PI,
-  [Orientation.UPLEFT]: 3 * Math.PI / 4
-}
+  [Orientation.UPLEFT]: (3 * Math.PI) / 4,
+};
 
 export const OrientationArray: Orientation[] = [
   Orientation.UP,
@@ -32,8 +32,8 @@ export const OrientationArray: Orientation[] = [
   Orientation.DOWN,
   Orientation.DOWNLEFT,
   Orientation.LEFT,
-  Orientation.UPLEFT
-]
+  Orientation.UPLEFT,
+];
 
 export function effectInLine(
   board: Board,
@@ -44,31 +44,31 @@ export function effectInLine(
   const orientation: Orientation =
     target instanceof PokemonEntity
       ? board.orientation(
-        pokemon.positionX,
-        pokemon.positionY,
-        target.positionX,
-        target.positionY,
-        pokemon,
-        target
-      )
-      : target
+          pokemon.positionX,
+          pokemon.positionY,
+          target.positionX,
+          target.positionY,
+          pokemon,
+          target
+        )
+      : target;
 
-  const targetsHit = new Set()
+  const targetsHit = new Set();
 
   const applyEffect = (x: number, y: number) => {
-    const value = board.getEntityOnCell(x, y)
+    const value = board.getEntityOnCell(x, y);
     if (value != null) {
-      targetsHit.add(value)
+      targetsHit.add(value);
     }
-    effect({ x, y, value })
-  }
+    effect({ x, y, value });
+  };
 
   switch (orientation) {
     case Orientation.UP:
       for (let y = pokemon.positionY + 1; y < board.rows; y++) {
-        applyEffect(pokemon.positionX, y)
+        applyEffect(pokemon.positionX, y);
       }
-      break
+      break;
 
     case Orientation.UPRIGHT:
       for (
@@ -76,15 +76,15 @@ export function effectInLine(
         x < board.columns && y < board.rows;
         x++, y++
       ) {
-        applyEffect(x, y)
+        applyEffect(x, y);
       }
-      break
+      break;
 
     case Orientation.RIGHT:
       for (let x = pokemon.positionX + 1; x < board.rows; x++) {
-        applyEffect(x, pokemon.positionY)
+        applyEffect(x, pokemon.positionY);
       }
-      break
+      break;
 
     case Orientation.DOWNRIGHT:
       for (
@@ -92,15 +92,15 @@ export function effectInLine(
         x < board.columns && y >= 0;
         x++, y--
       ) {
-        applyEffect(x, y)
+        applyEffect(x, y);
       }
-      break
+      break;
 
     case Orientation.DOWN:
       for (let y = pokemon.positionY - 1; y >= 0; y--) {
-        applyEffect(pokemon.positionX, y)
+        applyEffect(pokemon.positionX, y);
       }
-      break
+      break;
 
     case Orientation.DOWNLEFT:
       for (
@@ -108,15 +108,15 @@ export function effectInLine(
         x >= 0 && y >= 0;
         x--, y--
       ) {
-        applyEffect(x, y)
+        applyEffect(x, y);
       }
-      break
+      break;
 
     case Orientation.LEFT:
       for (let x = pokemon.positionX - 1; x >= 0; x--) {
-        applyEffect(x, pokemon.positionY)
+        applyEffect(x, pokemon.positionY);
       }
-      break
+      break;
 
     case Orientation.UPLEFT:
       for (
@@ -124,42 +124,42 @@ export function effectInLine(
         x >= 0 && y < board.rows;
         x--, y++
       ) {
-        applyEffect(x, y)
+        applyEffect(x, y);
       }
-      break
+      break;
   }
 
   if (target instanceof PokemonEntity && targetsHit.has(target) === false) {
     // should at least touch the original target
     // this can happen when target has an angle in between 45 degrees modulo, see https://discord.com/channels/737230355039387749/1098262507505848523
-    effect({ x: target.positionX, y: target.positionY, value: target })
+    effect({ x: target.positionX, y: target.positionY, value: target });
   }
 }
 
 export function getOrientation(x1: number, y1: number, x2: number, y2: number) {
-  let angle = Math.atan2(y2 - y1, x2 - x1)
+  let angle = Math.atan2(y2 - y1, x2 - x1);
   if (angle < 0) {
-    angle += 2 * Math.PI
+    angle += 2 * Math.PI;
   }
-  const quarterPi = Math.PI / 4
+  const quarterPi = Math.PI / 4;
   // logger.debug(angle);
   if (angle < quarterPi) {
-    return Orientation.RIGHT
+    return Orientation.RIGHT;
   } else if (angle < 2 * quarterPi) {
-    return Orientation.DOWNRIGHT
+    return Orientation.DOWNRIGHT;
   } else if (angle < 3 * quarterPi) {
-    return Orientation.DOWN
+    return Orientation.DOWN;
   } else if (angle < 4 * quarterPi) {
-    return Orientation.DOWNLEFT
+    return Orientation.DOWNLEFT;
   } else if (angle < 5 * quarterPi) {
-    return Orientation.LEFT
+    return Orientation.LEFT;
   } else if (angle < 6 * quarterPi) {
-    return Orientation.UPLEFT
+    return Orientation.UPLEFT;
   } else if (angle < 7 * quarterPi) {
-    return Orientation.UP
+    return Orientation.UP;
   } else if (angle < 8 * quarterPi) {
-    return Orientation.UPRIGHT
+    return Orientation.UPRIGHT;
   } else {
-    return Orientation.RIGHT
+    return Orientation.RIGHT;
   }
 }
